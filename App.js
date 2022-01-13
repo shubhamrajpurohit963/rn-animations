792ae05fafaba00eaa34a11fa2834827cc43a478
Shubham Rajpurohit <shubham@Shubhams-MacBook-Air.local>
2022-01-13 20:30:15 +0530

    Basics-01

---
+++ App.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import {
+  Animated,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+
+  const value = useState(new Animated.Value(0))[0]
+
+  const handleClick = () => {
+    Animated.timing(value, {
+      toValue: 140,
+      duration: 1000,
+      useNativeDriver: false
+    }).start()
+  }
+
+  return (
+    <SafeAreaView>
+      <Animated.View style={{ marginLeft: value }} >
+        <Text style={{ fontSize: 20 }}>Hello World</Text>
+      </Animated.View>
+
+      <TouchableOpacity
+        style={{
+          backgroundColor: 'black',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: 10,
+          margin: 10,
+          borderRadius: 10,
+        }}
+        onPress={handleClick}
+        >
+        <Text style={{color: 'white'}}>Start</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
